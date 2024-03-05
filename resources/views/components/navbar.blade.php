aa2e35bef8bf408eb8c1daa1aba73c0068cc098d
fabrizio medusa <fabrizio.medusa@gmail.com>
2024-03-05 20:46:13 +0100

    Creato Form Login/registrazione

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,36 +1,47 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">LOGO</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link active" aria-current="page" href=" {{ route ('homepage') }} ">Home</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+        @auth
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Benvenuto <strong>{{ auth()->user()->name }}</strong>!
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li><a class="dropdown-item"
+                    href="{{ route('articles.index')}}">Gestisti Articoli</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item"
+                    href="{{ route('categories.index')}}">Gestisti Categorie</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li>
+                    <form action="/logout" method="POST">
+                      @csrf
+                      <button class="dropdown-item" type="submit">Esci</button>
+                    </form>
+                  </li>
+                </ul>
+              </li>
+          </ul>
+          @else
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/login">Accedi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/register">Registrati</a>
+            </li>
+          </ul>
+          @endauth      
     </div>
   </div>
 </nav>
