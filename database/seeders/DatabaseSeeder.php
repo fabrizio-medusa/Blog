810455120b3188c2551fc5a4c086763827f7e9e2
fabrizio medusa <fabrizio.medusa@gmail.com>
2024-03-06 18:22:59 +0100

    pagine ripristinate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
 
          \App\Models\User::factory()->create([
              'email' => 'admin@admin.com',
+             'name' => 'admin',
              'password' => 'admin',
         ]);
     }
